8675d3b12d9f7330b3c9f9efce5effa8e5e0828b
Karthik Muralidharan <karthik.muralidharan@solarwinds.com>
2025-01-26 15:45:03 +0530

    add; brew commands

---
+++ list.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+########### Brew Install List ###########
+brew install ack
+brew install age
+brew install ankitpokhrel/jira-cli/jira-cli
+brew install automake
+brew install awscli
+brew install borkdude/brew/babashka
+brew install chezmoi
+brew install cmake
+brew install coreutils
+brew install direnv
+brew install docutils
+brew install fd
+brew install fish
+brew install flux
+brew install fzf
+brew install gh
+brew install git
+brew install gnupg
+brew install go
+brew install go-task
+brew install gradle
+brew install htop
+brew install hugo
+brew install hydra
+brew install imagemagick
+brew install jq
+brew install ktlint
+brew install kubectx
+brew install make
+brew install mergestat/mergestat/mergestat
+brew install mvnvm
+brew install neofetch
+brew install neovim
+brew install node
+brew install ollama
+brew install oven-sh/bun/bun
+brew install pandoc
+brew install plantuml
+brew install pyenv
+brew install rbenv
+brew install rcm
+brew install reattach-to-user-namespace
+brew install ripgrep
+brew install sdkman/tap/sdkman-cli
+brew install starship
+brew install subversion
+brew install telnet
+brew install tldr
+brew install tmux
+brew install tree
+brew install universal-ctags
+brew install watchman
+brew install wget
+brew install yarn
+brew install zsh
+brew install zsh-autosuggestions
+
+########### Brew Cask Install List ###########
+
+########### Mac App Store Install List ###########
+
+echo "And done..."
